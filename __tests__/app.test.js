6b54da87f54e33b66655c07030645f14dd37ff53
Sean <simpson.sean@outlook.com>
2021-08-02 16:35:23 -0700

    WORKDONE: Initial ACP on dev branch

---
+++ __tests__/app.test.js
@@ -0,0 +1,15 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('dog routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('inserts dog into dogs table', async () => {
+    const 
+
+  })
+});
